a15c9e6cad106ace7c59832672d66f6762b17f32
ebool <lastdroq@gmail.com>
2019-06-07 03:21:33 +0900

    add section tab

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -4,9 +4,49 @@ function refresh () {
   let toolbarCont = document.getElementById('toolbar-cont');
   removeChildNode(toolbarCont);
   drawToolbar(toolbarCont);
+
+  let sectionHeaderCont = document.getElementById('section-header-cont');
+  removeChildNode(sectionHeaderCont);
+  drawSectionHeader(sectionHeaderCont);
 }
 
 function removeChildNode (node) { while (node.firstChild) node.removeChild(node.firstChild); }
+
+function drawSectionHeader (node) {
+  if (window.route.length === 0) {
+    node.classList.add('none');
+    return;
+  }
+  else node.classList.remove('none');
+
+  for (let i of window.route) {
+    let tab = document.createElement('div');
+    tab.classList.add('tab');
+
+    let ic = document.createElement('img');
+    ic.src = '/assets/ic-img/html.png';
+    ic.classList.add('ic-default', 'mr4');
+
+    let name = document.createElement('div');
+    name.classList.add('mr4');
+    let paths = i.path.split('/');
+    name.innerText = `${paths[paths.length - 1]}.html`;
+
+    let close = document.createElement('span');
+    close.classList.add('material-icons', 'small-icon', 'close-icon');
+    close.innerText = 'close';
+
+    let border = document.createElement('div');
+    border.classList.add('bottom-border');
+
+    tab.appendChild(ic);
+    tab.appendChild(name);
+    tab.appendChild(close);
+    tab.appendChild(border);
+
+    node.appendChild(tab);
+  }
+}
 
 function drawToolbar (node) {
   let res = window.route.filter(i => i.isFocused)[0];
@@ -21,14 +61,14 @@ function drawToolbar (node) {
     let div = document.createElement('div');
     div.classList.add('flex');
 
-    let folder = document.createElement('img');
-    folder.src = i < path.length - 1 ? '/assets/ic-img/folder.png' : '/assets/ic-img/html.png';
-    folder.classList.add('ic-default', 'mr4');
+    let ic = document.createElement('img');
+    ic.src = i < path.length - 1 ? '/assets/ic-img/folder.png' : '/assets/ic-img/html.png';
+    ic.classList.add('ic-default', 'mr4');
 
     let name = document.createElement('div');
     name.innerText = i < path.length - 1 ? path[i] : path[i] + '.html';
 
-    div.appendChild(folder);
+    div.appendChild(ic);
     div.appendChild(name);
 
     node.appendChild(arrow);
